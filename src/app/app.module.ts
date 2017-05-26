1659093eade6b8b525016c5be32d21ca99cac698
Daniel Weber <dw@thedanielweber.com>
2017-05-26 10:57:50 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
