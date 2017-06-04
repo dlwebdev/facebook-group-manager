9ebd66d8f4b2fa715bbfab1223df66573aae5081
Daniel Weber <dw@thedanielweber.com>
2017-06-04 17:42:37 -0400

    Editing bills now working

--- src/app/bills/edit/edit.component.ts
+++ src/app/bills/edit/edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { BillService } from "../../services/bill.service";
+
+import { Bill } from '../bill';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
+  bill: Bill;
+  currentBillId: String;
+  sub: Object;
+  errorMessage: String;
 
-  constructor() { }
+  constructor(
+    private billService: BillService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.bill = new Bill();
+  }
 
   ngOnInit() {
+    this.getBillDetails();
+  }
+
+  getBillDetails() {
+    this.sub = this.route.params.subscribe(params => {
+
+      console.log("Checking params: ", params);
+
+      if (params['id'] !== undefined) {
+        let billId = params['id'];
+        this.currentBillId = billId;
+
+        this.billService.getBill(billId)
+          .subscribe(
+            bill => {
+              console.log("I was returned this bill from the backend: ", bill);
+              this.bill = bill;
+            },
+            error =>  this.errorMessage = <any>error
+          );
+
+      }
+    });
+  }
+
+
+  updateBill(): void {
+    this.billService.updateBill(this.bill)
+      .subscribe(
+        resp => {
+          this.router.navigate(['/bills/manage']);
+        },
+        error =>  this.errorMessage = <any>error
+      );
+
+  }
+
+
+  deleteBill(): void {
+    this.billService.deleteBill(this.bill._id)
+      .subscribe(
+        resp => {
+          this.router.navigate(['/bills/manage']);
+        },
+        error =>  this.errorMessage = <any>error
+      );
+
   }
 
 }
